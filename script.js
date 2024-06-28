5c700e7d7dce551ae5f58bc2be531b6abf44657f
ImHeroesKiller <148073026+ImHeroesKiller@users.noreply.github.com>
2024-06-28 17:01:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function initialize() {
     gapi.client.init({
-        apiKey: '551964834401-ho2pceck8pkiqub015s8v7ap2as4pib5.apps.googleusercontent.com'  // Ganti dengan API Key Anda
+        apiKey: 'AIzaSyBJbdfNaw7HJkDD1kqblfDp85Dfma7h3X8'  // Ganti dengan API Key Anda
     }).then(function() {
         return gapi.client.request({
             'path': 'https://sheets.googleapis.com/v4/spreadsheets/1EevPSckJw22gIwcENrg9stlrLzC-XNi4Br_lsOpk2I8/values/Sheet1!A2:C'
